ef7c5c2302905219ed11bac7f0b86a9f98f233e4
Maria Mozgunova <bagira_2003@inbox.ru>
2020-08-27 14:06:01 +0300

    Added Review and Comment classes

--- api/models.py
+++ api/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+def range_of_1_10(value):
+    if not (1 <= value <= 10):
+        raise ValidationError('Оценка должна быть в диапазоне от 1 до 10')
+
+class BaseForCommAndRev(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="comments",
+        verbose_name='Автор'
+    )
+    pub_date = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата создания'
+    )
+
+    def __str__(self):
+        return self.text
+
+
+class Comments(BaseForCommAndRev):
+    review = models.ForeignKey(
+        Review, 
+        on_delete=models.CASCADE, 
+        related_name="comments",
+        verbose_name='Отзыв'
+    )    
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'комментарии'
+
+
+class Review(BaseForCommAndRev):
+    title = models.ForeignKey(
+        Title, 
+        on_delete=models.CASCADE, 
+        related_name="reviews",
+        verbose_name='Произведение'
+    )
+    score = models.PositiveSmallIntegerField(validators=[range_of_1_10])
+    
+    class Meta:
+        verbose_name = 'отзыв'
+        verbose_name_plural = 'отзывы'
